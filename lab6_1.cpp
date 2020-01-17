901cc01e7606c9d7346a52b91e4c038c4dc99652
sathayu2544 <59278037+sathayu2544@users.noreply.github.com>
2020-01-17 15:32:55 +0700

    Update lab6_1.cpp

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
-int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
-    return 0;
+using namespace std;
+int main()
+{
+	float a = 20,b = 30;
+	float c = 4;
+	
+	double output = a - b/c;
+	cout << "output:" << output <<"\n";
+	return 0;
 }
